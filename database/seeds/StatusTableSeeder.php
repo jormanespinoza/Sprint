5d0571b20a44846bf5765241459c262dc449c0f1
Jorman Espinoza <jdaniel.espinoza89@gmail.com>
2017-07-25 23:15:36 -0400

    add status' descriptions

--- database/seeds/StatusTableSeeder.php
+++ database/seeds/StatusTableSeeder.php
@@ -14,22 +14,27 @@ class StatusTableSeeder extends Seeder
     {
         $status_pending = new Status();
         $status_pending->name = "Pendiente";
+        $status_pending->description = "Tarea pendiente por aprobación del Líder de Proyecto";
         $status_pending->save();
 
         $status_approved = new Status();
-        $status_approved->name = "Aprobado";
+        $status_approved->name = "Aprobada";
+        $status_approved->description = "Tarea aprobada por el Líder de Proyecto";
         $status_approved->save();
 
         $status_to_confirm = new Status();
         $status_to_confirm->name = "Por Confirmar";
+        $status_to_confirm->description = "Tarea realizada por el desarrollador, pendiente por confirmación del Líder de Proyecto";
         $status_to_confirm->save();
 
         $status_rejected = new Status();
-        $status_rejected->name = "Rechazado";
+        $status_rejected->name = "Rechazada";
+        $status_rejected->description = "Tarea rechazada por el Líder de Proyecto, requiere programación";
         $status_rejected->save();
 
         $status_confirmed = new Status();
-        $status_confirmed->name = "Confirmado";
+        $status_confirmed->name = "Confirmada";
+        $status_confirmed->description = "Tarea lista y confirmada por el Líder de Proyecto";
         $status_confirmed->save();
     }
 }
